57a9300a840316e75a374186a10801bc315a924d
rodrigordgfs <rodrigordgfs@gmail.com>
2025-06-15 12:46:15 -0300

    fix: Ajuste no componente IconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -3,14 +3,21 @@ import { LucideIcon } from "lucide-react";
 interface IconButtonProps {
   icon: LucideIcon;
   name: string;
+  disabled?: boolean;
   onClick: () => void;
 }
 
-const IconButton = ({ icon: Icon, name, onClick }: IconButtonProps) => {
+const IconButton = ({
+  icon: Icon,
+  name,
+  onClick,
+  disabled,
+}: IconButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="p-2 rounded-md cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 text-zinc-500 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100 transition-all duration-200 ease-in-out"
+      disabled={disabled}
+      className="p-2 rounded-md cursor-pointer hover:bg-zinc-100 dark:hover:bg-zinc-800 text-zinc-500 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-zinc-100 transition-all duration-200 ease-in-out disabled:opacity-50 disabled:cursor-not-allowed"
     >
       <Icon className="h-5 w-5" />
       <span className="sr-only">{name}</span>
